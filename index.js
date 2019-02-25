8c6a1d7bf228383c65c87390c9ad0b07c1fa5cd3
Brian DiRito <bdirito91@gmail.com>
2019-02-24 20:38:50 -0500

    redid the lab

--- index.js
+++ index.js
@@ -1,38 +1,28 @@
-// Write your code in this file!
-function scuberGreetingForFeet(someValue) {
-  let greeting;
-
-  if (someValue <= 400) {
-    greeting = "This one is on me!";
-  } else if (someValue > 2000 && someValue < 2500) {
-    greeting = "I will gladly take your thirty bucks."
-  } else if  (someValue > 2500){
-    greeting = "No can do."
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+    return "This one is on me!"
+  } else if (ride > 2000 && ride < 2500){
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
   }
-  return greeting
 }
 
-function ternaryCheckCity(city) {
-  let checker;
-
-  city === "NYC" ? (checker = "Ok, sounds good.") : (checker = "No go.");
-
-  return checker
+function ternaryCheckCity(destination){
+  return destination === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(tip) {
-  let charm;
-
-  switch (tip) {
-    case 'generous':
-      charm = "Thank you so much.";
-      break;
-    case 'not as generous':
-      charm = "Thank you.";
-      break;
-    default:
-      charm = "Bye."
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case "generous":
+      return "Thank you so much."
       break
+    
+    case "not as generous":
+      return "Thank you."
+      break
+      
+    default:
+      return "Bye."
   }
-  return charm
 }
